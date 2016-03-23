9477ed92c3f03d17bcb223a0ad3d6fb38d0cd26a
QingWang <Wang@QingWangdeMac-mini.local>
2016-03-23 17:37:52 +0800

    chushuhua

---
+++ MyBibibi/MyBibibi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBibibi
+//
+//  Created by QingWang on 16/3/23.
+//  Copyright © 2016年 iyuanxu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyBibibi/MyBibibi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyBibibi
+//
+//  Created by QingWang on 16/3/23.
+//  Copyright © 2016年 iyuanxu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
